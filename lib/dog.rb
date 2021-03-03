01a15b24b8d1189c9ba1fdc9031407e84db0e91c
Erin Anderson <e.anderson1226@gmail.com>
2021-03-02 20:40:20 -0500

    commit tree cfadde5ee7c1d0851ac3fe3ec0cb735a1d2af06b

---
+++ lib/dog.rb
@@ -0,0 +1,58 @@
+class Dog
+
+    attr_accessor :name, :breed
+    attr_reader :id
+
+    def initialize(id=nil, name, breed)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create(name:, breed:)
+        dog = Dog.new(name, breed)
+        dog.save
+        dog
+    end
+
+    def save
+        if self.id
+            self.update
+        else
+            sql = <<-SQL
+            INSERT INTO dogs (name, breed) VALUES (?, ?)
+            SQL
+            DB[:conn].execute(sql, self.name, self.breed)
+            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        end
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE IF NOT EXISTS dogs INSERT INTO dogs (name, breed) PRIMARY INTEGER KEY id, TEXT name, TEXT breed
+        SQL
+    end
+
+    def self.drop_table
+        sql <<-SQL
+            DROP TABLE IF EXISTS dogs
+        SQL
+    end
+
+    def update
+        sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+
+    def self.find_by_name(name)
+    end
+
+    def self.find_or_create_by
+    end
+
+    def self.new_from_db
+    end
+
+    def self.find_by_id
+    end
+end
